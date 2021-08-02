a9a7d0936f6c6f5c28412d986f1b95313824952d
Francisca Kátia Barbosa de Souza <fkbs04@hotmail.com>
2021-08-02 17:07:59 -0300

    Projeto 2 - Construtor

---
+++ Aula06_2/Pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula06_2
+{
+    class Pessoa
+    {
+        private string Nome { get; set; }
+        private double SalarioBruto { get; set; }
+        private double Imposto { get; set; }
+
+        public Pessoa(string nome, double salB, double imposto)
+        {
+            this.Nome = nome;
+            this.SalarioBruto = salB;
+            this.Imposto = imposto;
+        }
+
+
+
+
+
+
+
+
+
+
+        public double SalarioLiquido()
+        {
+            double valorSalario = SalarioBruto - Imposto;
+
+            return valorSalario;
+        }
+
+
+
+        private double valorPorcentagem;
+        public void AumentarSalario(double porcentagem)
+        {
+            valorPorcentagem = (SalarioBruto * porcentagem / 100) + SalarioLiquido();
+        }
+
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}\n" +
+                $"Salario Bruto: {SalarioBruto:F2}\n" +
+                $"Imposto: {Imposto:F2}\n" +
+                $"Dados atualizados:{Nome} , R$ {valorPorcentagem:F2}";
+
+        }
+    }
+}
---
+++ Aula06_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Aula06_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Digite o nome da Pessoa: ");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o Salario Bruto da Pessoa: ");
+
+            double salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do Imposto: ");
+
+            double imposto = double.Parse(Console.ReadLine());
+
+
+            Pessoa p = new Pessoa(nome, salario, imposto);
+            
+
+
+            p.SalarioLiquido();
+
+            Console.WriteLine("Digite a porcentagem recebida");
+            double porcentagem = double.Parse(Console.ReadLine());
+
+            p.AumentarSalario(porcentagem);
+
+            Console.WriteLine("---------------------");
+
+
+            Console.WriteLine(p);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
