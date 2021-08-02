923d97a90934ca60983a3bb32944af12ca982761
Francisca Kátia Barbosa de Souza <fkbs04@hotmail.com>
2021-08-02 15:36:17 -0300

    Adicionar arquivos de projeto.

---
+++ Aula06_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aula06_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t = new Triangulo();
+
+       
+
+            Console.WriteLine("Entre com as medidas do triângulo x:");
+            double xA = double.Parse(Console.ReadLine());
+            double xB = double.Parse(Console.ReadLine());
+            double xC = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Entre com as medidas do triângulo y:");
+            //double yA = double.Parse(Console.ReadLine());
+            //double yB = double.Parse(Console.ReadLine());
+            //double yC = double.Parse(Console.ReadLine());
+
+
+
+
+            double areaX = t.Area(xA, xB, xC);
+
+            Console.WriteLine(t);            
+
+
+            //double areaY = t.Area(yA, yB, yC);
+
+            //Console.WriteLine(t);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula06_1/Triangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06_1
+{
+   public class Triangulo
+    {
+        public double ValorA { get; set; }
+        public double ValorB { get; set; }
+        public double ValorC { get; set; }
+
+        private double area;
+        public double Area(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2.0;
+
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));    
+   
+            return area;
+
+        }
+
+        public override string ToString()
+        {
+            return $"A Area do Tringulo é: {area}";
+               
+        }
+
+  
+    }
+}
